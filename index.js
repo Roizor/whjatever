25bab2a5a0a5da0abbc21cdb6d653d582ff95c17
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-17 22:02:35 -0500

    GREAT

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const config = require('./config.json')
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
