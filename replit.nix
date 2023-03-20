902b6b4e5a4baa56c519cc9fd8903eb38d79b6cf
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-20 01:06:13 +0000

    sdfsedf

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+	deps = [
+		pkgs.ffmpeg.bin
+  pkgs.sudo
+  pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+	];
+}
--- web/startup.js
+++ web/startup.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const a = express()
 
-a.get('/jklsdfgjkldjklsdfgjkergkljergergergiopuqwertuiop23iou345fgxcv', (req, res) => {
-  process.exit(0)
-})
-
 a.get('/*', (req, res) => {
-  res.sendStatus(404)
+	res.sendStatus(404)
 })
 
 a.listen(8900)
