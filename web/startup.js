1b768ae75d5916f33d35b6c7eb65fbeffc9d280a
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-19 19:56:27 -0500

    horrible

--- web/startup.js
+++ web/startup.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const a = express()
 
+a.get('/jklsdfgjkldjklsdfgjkergkljergergergiopuqwertuiop23iou345fgxcv', (req, res) => {
+  process.exit(0)
+})
+
 a.get('/*', (req, res) => {
-  res.sendStatus(200)
+  res.sendStatus(404)
 })
 
 a.listen(8900)
